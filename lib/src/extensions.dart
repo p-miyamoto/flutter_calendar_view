e3c04b04c3bc61c01a4ff17832e2310b0ce970d5
宮本匠一 <s-miyamoto@pai-r.com>
2023-10-10 13:04:14 +0900

    Revert "日付の並び替えを無効化"
    This reverts commit e7569d1bdec9b6d1fa164c0c8a6622721c8ac237.

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -169,6 +169,18 @@ extension MyList on List<CalendarEventData> {
   // Below function will add the new event in sorted manner(startTimeWise) in
   // the existing list of CalendarEventData.
   void addEventInSortedManner(CalendarEventData event) {
-    this.add(event);
+    var addIndex = -1;
+    for (var i = 0; i < this.length; i++) {
+      if (event.startTime!.compareTo(this[i].startTime!) <= 0) {
+        addIndex = i;
+        break;
+      }
+    }
+
+    if (addIndex > -1) {
+      this.insert(addIndex, event);
+    } else {
+      this.add(event);
+    }
   }
 }
